3fe6fb5b8a02e529a74d05b6eb93645787fd3c36
Lucas Passos <lucasplpx@gmail.com>
2022-05-05 21:13:19 -0300

    feat: cartoes de transacoes

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { HighlightCard } from '../../components/HighlightCard';
+import { TransactionCard } from '../../components/TransactionCard';
 
 import {
   Container,
@@ -12,6 +13,8 @@ import {
   UserName,
   UserWrapper,
   HighlightCards,
+  Transactions,
+  Title,
 } from './styles';
 
 export const Dashboard = () => {
@@ -55,6 +58,11 @@ export const Dashboard = () => {
           lastTransaction='01 à 16 de abril'
         />
       </HighlightCards>
+
+      <Transactions>
+        <Title>Listagem</Title>
+        <TransactionCard />
+      </Transactions>
     </Container>
   );
 };
