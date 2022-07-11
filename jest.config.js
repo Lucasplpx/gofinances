ba5393b0329813c63e3566acd6733bb05df1a477
Lucas Passos <lucasplpx@gmail.com>
2022-07-11 19:57:02 -0300

    configuracoes jest

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+const config = {
+  preset: 'jest-expo',
+  testPathIgnorePatterns: [
+    'node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg)',
+  ],
+  setupFilesAfterEnv: [
+    '@testing-library/jest-native/extend-expect',
+    'jest-styled-components',
+  ],
+};
+
+module.exports = config;
--- src/__tests__/screens/profile.spec.tsx
+++ src/__tests__/screens/profile.spec.tsx
@@ -4,28 +4,30 @@ import { render } from '@testing-library/react-native';
 
 import { Profile } from '../../screens/Profile';
 
-test('check if show correctly user input name placeholder', () => {
-  const { getByPlaceholderText } = render(<Profile />);
+describe('Profile Screen', () => {
+  it('should have correctly correctly in user name input', () => {
+    const { getByPlaceholderText } = render(<Profile />);
 
-  const inputName = getByPlaceholderText('Nome');
+    const inputName = getByPlaceholderText('Nome');
 
-  expect(inputName).toBeTruthy();
-});
+    expect(inputName).toBeTruthy();
+  });
 
-test('checks if user data has been loaded', () => {
-  const { getByTestId } = render(<Profile />);
+  it('should be load user data', () => {
+    const { getByTestId } = render(<Profile />);
 
-  const inputName = getByTestId('input-name');
-  const inputSurname = getByTestId('input-surname');
+    const inputName = getByTestId('input-name');
+    const inputSurname = getByTestId('input-surname');
 
-  expect(inputName.props.value).toEqual('Lucas');
-  expect(inputSurname.props.value).toEqual('Passos');
-});
+    expect(inputName.props.value).toEqual('Lucas');
+    expect(inputSurname.props.value).toEqual('Passos');
+  });
 
-test('checks if title render correctly', () => {
-  const { getByTestId } = render(<Profile />);
+  it('should exist title correctly', () => {
+    const { getByTestId } = render(<Profile />);
 
-  const textTitle = getByTestId('text-title');
+    const textTitle = getByTestId('text-title');
 
-  expect(textTitle.props.children).toContain('Perfil');
+    expect(textTitle.props.children).toContain('Perfil');
+  });
 });
